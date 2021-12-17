d153e3f7e91ce0e14302121d2d94a6cfe09e3287
Alexander Lansdorp <alexlansdorprs@gmail.com>
2021-12-16 20:54:32 -0500

    Link implemented

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout(props) {
@@ -7,7 +7,7 @@ export default function Layout(props) {
         <React.Fragment>
             <Header />
             {props.children}
-            <Footer />
+            {/* <Footer /> */}
         </React.Fragment>
     )
 }
